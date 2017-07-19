09708a4e941c839f61786ddfcbe500ca51c5952c
Ichinose Shogo <shogo82148@gmail.com>
2017-07-19 14:52:34 +0900

    add name option

--- reporter.py
+++ reporter.py
@@ -6,10 +6,16 @@ import json
 import sys
 import string
 import os
+import argparse
 import boto3
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--name', help="name of alarm")
+    args = parser.parse_args()
+
     report = json.load(sys.stdin)
+    name = args.name or (report["hostname"] + ": " + report["command"])
 
     topic_arn = os.environ.get("HORENSO_TOPIC_ARN", None)
     if not topic_arn:
@@ -22,7 +28,7 @@ def main():
 
     region = "Unknown"
     template = string.Template(u"""Songmu/horenso report
-$hostname: $commandArgs
+$name
 - Result:     $result
 - StartAt:    $startAt
 - EndAt:      $endAt
@@ -35,10 +41,10 @@ $stdout
 Stderr:
 $stderr
 """)
-    text_message = template.substitute(**report)
+    text_message = template.substitute(name=name, **report)
 
     json_message = json.dumps({
-        "AlarmName": report["hostname"] + ": " + report["command"],
+        "AlarmName": name,
         "AlarmDescription": " ".join(report["commandArgs"]),
         "AWSAccountId": "0",
         "NewStateValue": "OK" if report["exitCode"] == 0 else "ALARM",
